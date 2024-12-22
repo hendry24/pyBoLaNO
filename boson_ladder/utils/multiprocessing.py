c4eeb9e89bfec9d798a9b52619ba1bd39075fef6
hendry24 <hend24lelen@gmail.com>
2024-12-22 13:10:47 +0700

    Add CustomDict for multiprocessing

--- boson_ladder/utils/multiprocessing.py
+++ boson_ladder/utils/multiprocessing.py
@@ -1,9 +1,23 @@
 import os
 
-mp_config = {
-    "enable" : True,
-    "num_cpus" : os.cpu_count(),
-    "min_num_args" : 2
-    } 
+class CustomDict(dict):
+    def __setitem__(self, key, value):
+        valid_keys = ["enable", "num_cpus", "min_num_args"]
+        if key not in valid_keys:
+            msg = f"The key [{key}] is not valid. Valid keys: {valid_keys}."
+            raise KeyError(msg)
+
+        if key == "min_num_args":
+            if value >= 2:
+                super().__setitem__(key, value)
+            else:
+                super().__setitem__(key, 2)
+        else:
+            super().__setitem__(key, value)
+
+mp_config = CustomDict()
+mp_config["enable"] = True
+mp_config["num_cpus"] = os.cpu_count()
+mp_config["min_num_args"] = 2
     # Skip multiprocessing if the number of elements is small,
     # in which case a single core execution is enough.
